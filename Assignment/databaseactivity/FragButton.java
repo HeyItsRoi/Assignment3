ea4b4fd9f1341c5415728931ec2b31b1c1f72eba
HeyItsRoi <caballero.roijan@gmail.com>
2016-11-10 13:26:05 -0500

    Add files via upload

---
+++ Assignment/databaseactivity/FragButton.java
@@ -0,0 +1,23 @@
+package com.example.cstuser.databaseactivity;
+
+/**
+ * Created by cstuser on 11/10/2016.
+ */
+        import android.app.Fragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+public class FragButton extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        //---Inflate (or expand or fill) the layout for this fragment---
+        return inflater.inflate(R.layout.frag_buttons, container, false);
+    }
+}
+
---
+++ Assignment/databaseactivity/FragmentActivity.java
@@ -0,0 +1,15 @@
+package com.example.cstuser.databaseactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cstuser on 11/10/2016.
+ */
+public class FragmentActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_activiry);
+    }
+}
